cb570c486fd013aa4d7a0b2e855c00a8ef2c879d
Miguel Carvalho <dev@sundathr.ee>
2025-05-15 13:35:04 -0300

    minimal theme

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,9 @@
 [System.Console]::OutputEncoding = [System.Console]::InputEncoding = [System.Text.Encoding]::UTF8
-New-Alias ll ls -Force      # ll -> detailed dir listing
+New-Alias ll ls -Force      			# ll -> detailed dir listing
 function .. { cd .. }                           # Go up one directory
 function ... { cd ../.. }                       # Go up two directories
 function cl { clear }                           # Clear screen
-function rm { remove-item }                     # Remove
+function rm { remove-item }            		# Remove
 function home { cd $env:userprofile }
 function touch {if((Test-Path -Path ($args[0])) -eq $false) {Set-Content -Path ($args[0]) -Value ($null)} else {(Get-Item ($args[0])).LastWriteTime = Get-Date } }
 function rmrf {
@@ -24,4 +24,4 @@ function y {
 }
 Invoke-Expression (&starship init powershell)
 $Env:KOMOREBI_CONFIG_HOME = '$Env:USERPROFILE/.config/komorebi'
-$Env:XGD_CONFIG_HOME = '$Env:USERPROFILE/.config'
+$Env:XDG_CONFIG_HOME = '$Env:USERPROFILE/.config'
